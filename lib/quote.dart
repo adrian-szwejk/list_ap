abec9d2622271e8cdda673968cc99ef74e75fa80
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-06-08 19:51:56 -0500

    created quote class to connect quotes to authors

---
+++ lib/quote.dart
@@ -0,0 +1,13 @@
+class Quote {
+  //Variables
+  String? text;
+  String? author;
+
+  //Constructor
+  Quote(String t, String a) {
+    text = t;
+    author = a;
+  }
+}
+
+//Quote myQuote = Quote("this is an example", "spaghetti man");  EXAMPLE
